07ed017542fe0d4fc255f63cad3c16bc7a5faa77
Lenny Zeitoun <30462059+ZeitounCorp@users.noreply.github.com>
2020-05-22 18:23:58 +0300

    Create Alphabet&More.ino

---
+++ Alphabet&More.ino
@@ -0,0 +1,256 @@
+/**
+ * author: Lenny Zeitoun @zcorp
+ * date: 05/21/20
+ * description: Simple Seven Segment displaying some characters
+ */
+ 
+void setup() {
+ // .. doStuff()
+}
+
+void loop() {
+ // .. doThings()
+}
+ 
+void playPause() {
+  digitalWrite(pin_a,LOW);
+  digitalWrite(pin_b,LOW);
+  digitalWrite(pin_c,LOW);
+  digitalWrite(pin_d,LOW);
+  digitalWrite(pin_e,LOW);
+  digitalWrite(pin_f,LOW);
+  digitalWrite(pin_g,LOW);
+  digitalWrite(pin_dp,LOW);
+  delay(500);
+}
+
+void playA() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,LOW);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,HIGH);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playB() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,HIGH);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playC() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,LOW);
+  digitalWrite(pin_c,LOW);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,LOW);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playD() {
+  digitalWrite(pin_a,LOW);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,LOW);
+  digitalWrite(pin_g,HIGH);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playE() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,LOW);
+  digitalWrite(pin_c,LOW);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,HIGH);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playF() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,LOW);
+  digitalWrite(pin_c,LOW);
+  digitalWrite(pin_d,LOW);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,HIGH);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playG() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,LOW);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,HIGH);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playH() {
+  digitalWrite(pin_a,LOW);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,LOW);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,HIGH);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playI() {
+  digitalWrite(pin_a,LOW);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,LOW);
+  digitalWrite(pin_e,LOW);
+  digitalWrite(pin_f,LOW);
+  digitalWrite(pin_g,LOW);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playJ() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,LOW);
+  digitalWrite(pin_g,LOW);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playL() {
+  digitalWrite(pin_a,LOW);
+  digitalWrite(pin_b,LOW);
+  digitalWrite(pin_c,LOW);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,LOW);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playN() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,LOW);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,LOW);
+  digitalWrite(pin_dp,HIGH);
+}
+
+void playO() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,LOW);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playP() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,LOW);
+  digitalWrite(pin_d,LOW);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,HIGH);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playQ() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,LOW);
+  digitalWrite(pin_dp,HIGH);
+}
+
+void playR() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,LOW);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,HIGH);
+  digitalWrite(pin_dp,HIGH);
+}
+
+void playS() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,LOW);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,LOW);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,HIGH);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playU() {
+  digitalWrite(pin_a,LOW);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,LOW);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playV() {
+  digitalWrite(pin_a,LOW);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,LOW);
+  digitalWrite(pin_dp,HIGH);
+}
+
+void playY() {
+  digitalWrite(pin_a,LOW);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,HIGH);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,LOW);
+  digitalWrite(pin_f,HIGH);
+  digitalWrite(pin_g,HIGH);
+  digitalWrite(pin_dp,LOW);
+}
+
+void playZ() {
+  digitalWrite(pin_a,HIGH);
+  digitalWrite(pin_b,HIGH);
+  digitalWrite(pin_c,LOW);
+  digitalWrite(pin_d,HIGH);
+  digitalWrite(pin_e,HIGH);
+  digitalWrite(pin_f,LOW);
+  digitalWrite(pin_g,HIGH);
+  digitalWrite(pin_dp,LOW);
+}
